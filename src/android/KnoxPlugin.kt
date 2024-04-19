08c749d2e9e9783e294cab8221d7bf8c84f06cd4
Josh Noel <jnoel@healthrecoverysolutions.com>
2024-04-19 11:56:38 -0500

    add core knoxsdk lib file, copy over basic actions for reboot and shutdown

--- src/android/KnoxPlugin.kt
+++ src/android/KnoxPlugin.kt
@@ -5,8 +5,14 @@ import org.apache.cordova.CordovaPlugin
 import org.json.JSONArray
 import org.json.JSONObject
 import timber.log.Timber
+import android.content.Context
+import com.samsung.android.knox.custom.CustomDeviceManager
+import com.samsung.android.knox.custom.SystemManager
+import com.samsung.android.knox.EnterpriseDeviceManager
+import com.samsung.android.knox.devicesecurity.PasswordPolicy
 
-private const val ACTION_TEST = "test"
+private const val ACTION_SHUTDOWN = "shutdown"
+private const val ACTION_REBOOT = "reboot"
 
 class KnoxPlugin : CordovaPlugin() {
 
@@ -33,9 +39,39 @@ class KnoxPlugin : CordovaPlugin() {
 	): Boolean {
 		Timber.v("execute action '$action'")
 		when (action) {
-			ACTION_TEST -> {
+			ACTION_SHUTDOWN -> {
 				cordova.threadPool.execute {
-					callbackContext.success("test!")
+					try {
+						// With Knox SDK 3.x this functionality now requires a premium KPE license for the SDK, which we might not get,
+						// so it could throw an exception if we try to use this, but keeping it just in case we do go with that
+						// license at some point:
+						// java.lang.RuntimeException: Unable to start receiver com.hrs.patientconnectknoxservice.PatientConnectInterface:
+						// java.lang.SecurityException: Admin  does not have com.samsung.android.knox.permission.KNOX_CUSTOM_SYSTEM OR
+						// com.sec.enterprise.knox.permission.CUSTOM_SYSTEM
+						CustomDeviceManager cdm = CustomDeviceManager.getInstance()
+						SystemManager kcsm = cdm.getSystemManager()
+						kcsm.powerOff()
+						callbackContext.success()
+					} catch (Exception e) {
+						val errorMessage = "Failed to power off device"
+						Timber.e(errorMessage, e)
+						callbackContext.error(errorMessage)
+					}
+				}
+			}
+
+			ACTION_REBOOT -> {
+				cordova.threadPool.execute {
+					try {
+						EnterpriseDeviceManager edm = EnterpriseDeviceManager.getInstance(context)
+						PasswordPolicy passwordPolicy = edm.getPasswordPolicy()
+						passwordPolicy.reboot(null)
+						callbackContext.success()
+					} catch (Exception e) {
+						val errorMessage = "Failed to reboot device"
+						Timber.e(errorMessage, e)
+						callbackContext.error(errorMessage)
+					}
 				}
 			}
 
