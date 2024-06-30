fe7144d3c1d5990c23a41616a784b2423b662c7b
saradelarosa <sarasailors@gmail.com>
2024-06-30 12:15:40 -0400

    Updating with API Authentication and fixing reboot

--- src/android/KnoxPluginEnabled.kt
+++ src/android/KnoxPluginEnabled.kt
@@ -5,18 +5,23 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Context.ALARM_SERVICE
+import android.content.Context.TELEPHONY_SERVICE
 import android.content.Intent
-import com.samsung.android.knox.EnterpriseDeviceManager
+import android.os.Build
+import android.telephony.TelephonyManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.hrs.patient.BuildConfig
 import com.samsung.android.knox.custom.CustomDeviceManager
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaPlugin
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 import timber.log.Timber
-import android.telephony.TelephonyManager
-import android.content.Context.TELEPHONY_SERVICE
-import android.os.Build
-import com.hrs.patient.BuildConfig
+
 
 private const val KNOX_ENABLED = true
 private const val ACTION_IS_ENABLED = "isEnabled"
@@ -24,9 +29,13 @@ private const val ACTION_SHUTDOWN = "shutdown"
 private const val ACTION_REBOOT = "reboot"
 private const val ACTION_GET_VERSION_INFO = "getVersionInfo"
 private const val ACTION_GET_IMEI = "getIMEI"
+private const val ACTION_ACCESS_KNOXAPI = "authenticateAPI"
 
 private const val KEY_KNOX_APP_VERSION = "knoxAppVersion"
 private const val TIME_SPAN_24_HOURS_MS = 4 * 60 * 60 * 1000
+private var imei: String = "" // Device IMEI that would be saved after the permissions prompts and directly pulled from Telephony
+private var bearerToken: String = "" // token needed for auth; expires and needs to be regenerated
+private var deviceId: String = "" // this is different from the IMEI; it's specific to Knox Manage
 
 class KnoxPlugin : CordovaPlugin() {
 
@@ -124,7 +133,11 @@ class KnoxPlugin : CordovaPlugin() {
                 callbackContext.error(errorMessage)
                 return
             }
-            val imei = telephonyManager.imei
+            imei = telephonyManager.imei
+
+            // Calling to authenticate the API after getting teh IMEI the first time so we are set; this may change depending on expiration
+            authenticateAPI();
+
             Timber.v("Retrieved IMEI: '$imei'")
             callbackContext.success(imei)
         } catch (ex: Exception) {
@@ -134,6 +147,8 @@ class KnoxPlugin : CordovaPlugin() {
         }
     }
 
+    // This is not implemented in monorepo yet b/c we didn't use it anymore in PCKS; there is a ticket for product to decide on this
+    // https://healthrecoverysolutions.atlassian.net/browse/DEV-11229
     private fun shutdown(callbackContext: CallbackContext) {
         try {
             // With Knox SDK 3.x this functionality now requires a premium KPE license for the SDK, which we might not get,
@@ -164,9 +179,34 @@ class KnoxPlugin : CordovaPlugin() {
     }
 
     private fun performReboot() {
-        val edm = EnterpriseDeviceManager.getInstance(cordova.context)
-        val passwordPolicy = edm.passwordPolicy
-        passwordPolicy.reboot(null)
+        val context = cordova.context
+        val url = "https://us02.manage.samsungknox.com/emm/oapi/mdm/commonOTCServiceWrapper/sendDeviceControlForRebootDevice"
+        val requestQueue = Volley.newRequestQueue(context)
+
+        val stringRequest = object: StringRequest(Request.Method.POST, url,
+            Response.Listener<String> { response ->
+                Timber.v("Reboot with Knox Manage API Response: '$response'")
+            },
+            Response.ErrorListener { error ->
+                Timber.e("Reboot with Knox Manage API Response Error", error);
+            }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["cache-control"] = "no-cache"
+                headers["content-type"] = "application/x-www-form-urlencoded"
+                headers["Authorization"] = "bearer $bearerToken"
+                return headers
+            }
+
+            // DeviceID != IMEI
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["deviceId"] = deviceId
+                return params
+            }
+        }
+
+        requestQueue.add(stringRequest)
     }
 
     private fun startRebootAlarm() {
@@ -194,4 +234,81 @@ class KnoxPlugin : CordovaPlugin() {
             Timber.e(ex, "failed to set reboot alarm due to unknown exception")
         }
     }
+
+    private fun authenticateAPI() {
+        try {
+            val context = cordova.context
+            Timber.d("Authenticating Knox Manage API")
+            val url =
+                "https://us02.manage.samsungknox.com/emm/oauth/token?grant_type=client_credentials&client_id=healthrecoverysolutions@development.healthrecoverysolutions.com&client_secret=HRSistheBest123!"
+            val requestQueue = Volley.newRequestQueue(context)
+            val stringRequest = object : StringRequest(
+                Method.POST, url,
+                Response.Listener { response ->
+                    Timber.v("Authenticating Knox Manage API response: $response")
+                    try {
+                        val responseObject = JSONObject(response)
+                        bearerToken = responseObject["access_token"].toString()
+
+                        // Getting device ID and passing along authentication
+                        if (deviceId == "") {
+                            getDeviceId()
+                        }
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+
+                },
+                Response.ErrorListener { error ->
+                    Timber.e("Authenticating Knox Manage API response error", error);
+                }) {
+            }
+
+            requestQueue!!.add(stringRequest)
+        } catch (ex: SecurityException) {
+            val errorMessage = "Failed to authenticate Knox Manage API: $ex"
+            Timber.e(errorMessage, ex)
+        }
+    }
+
+    // This is not the IMEI but it is used throughout the Knox Manage API
+    private fun getDeviceId() {
+        try {
+            val context = cordova.context
+            Timber.d("Getting DeviceId Knox Manage API")
+            val url = "https://us02.manage.samsungknox.com/emm/oapi/device/selectDeviceInfoByImei"
+            val requestQueue = Volley.newRequestQueue(context)
+
+            val stringRequest = object : StringRequest(
+                Method.POST, url,
+                Response.Listener { response ->
+                    Timber.v("Getting DeviceId Knox Manage API Response: '$response'")
+                    val responseObject = JSONObject(response)
+                    val resultValue = responseObject.getJSONObject("resultValue")
+                    deviceId = resultValue.getString("deviceId")
+                },
+                Response.ErrorListener { error ->
+                    Timber.e("Getting DeviceId Knox Manage API Response Error", error);
+                }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["cache-control"] = "no-cache"
+                    headers["content-type"] = "application/x-www-form-urlencoded"
+                    headers["Authorization"] = "bearer $bearerToken"
+                    return headers
+                }
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String, String>()
+                    params["imei"] = imei
+                    return params
+                }
+            }
+
+            requestQueue.add(stringRequest)
+        } catch (ex: SecurityException) {
+            val errorMessage = "Failed to get DeviceID from the Knox Manage API: $ex"
+            Timber.e(errorMessage, ex)
+        }
+    }
 }
+
