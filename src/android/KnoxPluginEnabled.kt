3833de97431deefcbbfea1a6d31ff24e1b342894
Brice Powell <bpowell@healthrecoverysolutions.com>
2024-07-08 14:25:27 -0400

    Fix imports

--- src/android/KnoxPluginEnabled.kt
+++ src/android/KnoxPluginEnabled.kt
@@ -23,6 +23,9 @@ import org.json.JSONObject
 import timber.log.Timber
 import android.provider.Settings;
 import androidx.core.content.ContextCompat.startActivity
+import com.samsung.android.knox.license.KnoxEnterpriseLicenseManager
+import com.samsung.android.knox.license.LicenseResult
+import com.samsung.android.knox.license.LicenseResultCallback
 
 
 private const val KNOX_ENABLED = true
@@ -32,7 +35,7 @@ private const val ACTION_SHUTDOWN = "shutdown"
 private const val ACTION_REBOOT = "reboot"
 private const val ACTION_GET_VERSION_INFO = "getVersionInfo"
 private const val ACTION_GET_IMEI = "getIMEI"
-private const val KEY_KNOX_APP_VERSION = "knoxAppVersion"
+private const val KEY_KNOX_APP_VERSION = "knoxAppVersonLicenseResultion"
 private const val TIME_SPAN_24_HOURS_MS = 4 * 60 * 60 * 1000
 private const val getDeviceIDURL = "https://us02.manage.samsungknox.com/emm/oapi/device/selectDeviceInfoByImei"
 private const val authenticateAPIURL = "https://us02.manage.samsungknox.com/emm/oauth/token?grant_type=client_credentials&client_id=healthrecoverysolutions@development.healthrecoverysolutions.com&client_secret=HRSistheBest123!"
